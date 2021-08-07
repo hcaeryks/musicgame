7b98a6e1d32df394364c6bc7abb1161d1c58c87f
OverMan-647 <54404011+OverMan-647@users.noreply.github.com>
2021-08-06 23:02:04 -0300

    Menu Added, still in development

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
 from PPlay.window import Window
 from screens.game import Game
+from screens.menu import Menu
 
-GAME_STATE = 4
+GAME_STATE = 0
 window = Window(1920, 1080)
 scrGame = Game(window)
-    
+scrMenu = Menu(window)
+
+# 0 - Menu
+# 1 - Opções
+# 2 - Seleção
+# 3 - Jogo
+# 4 - Resultados
 switcher = {
-    4: scrGame.draw
+    0: scrMenu.run,
+    3: scrGame.draw
 }
 
 while 1:
